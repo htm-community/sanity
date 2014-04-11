8ba27e8b1bceb056d78d659c57417a6b1405acd4
Felix Andrews <felix@nfrac.org>
2014-04-11 22:43:32 +0800

    ui controls

--- src/comportex/viz.cljs
+++ src/comportex/viz.cljs
@@ -5,12 +5,8 @@
             [strokes :refer [d3]]
             [goog.dom :as dom]
             [goog.events :as events]
-            [cljs.core.async :refer [put! chan <! timeout]])
+            [cljs.core.async :refer [put! chan <! alts! timeout]])
   (:require-macros [cljs.core.async.macros :refer [go]]))
-
-(enable-console-print!)
-
-(strokes/bootstrap)
 
 ;; initial CLA region
 (def numb-bit-width 64)
@@ -45,8 +41,39 @@
   (enc/map-encoder numb-bit-width
                    (enc/number-linear numb-bit-width numb-domain numb-span)))
 
+(def input-state (atom (vec (repeat n-in-items (/ numb-max 2)))))
+(def r-state (atom r-init))
+(def sim-go? (atom false))
+(def sim-step-ms (atom 1000))
+(def animation-go? (atom false))
+(def animation-step-ms (atom 1000))
+(def display-options (atom {}))
 
-;; graphic display
+(defn sim-step!
+  []
+  (let [newin (swap! input-state (partial add-noise 5))
+        newbits (efn newin)]
+    (println (str "inputs " newin))
+    (println (str "bits " (sort newbits)))
+    (let [newr (swap! r-state p/pooling-step newbits)
+          newom (:overlaps newr)
+          newac (:active-columns newr)]
+      (println "ac " (sort newac))
+      (println "om " (sort newom)))))
+
+(defn run-sim
+  []
+  (go
+   (while @sim-go?
+     (sim-step!)
+     (<! (timeout @sim-step-ms)))))
+
+;; GRAPHIC DISPLAY
+
+(enable-console-print!)
+
+(strokes/bootstrap)
+
 (def width 960)
 (def bitpx 5)
 (def inbits-height (* bitpx bit-width))
@@ -54,29 +81,26 @@
 (def height (max inbits-height rgn-height))
 
 (def inbits-svg
-  (-> d3 (.select "#viz-node") (.append "svg")
+  (-> d3 (.select "#viz") (.append "svg")
       (.attr {:width width :height height})
       (.append "g")
       (.attr {:id "inbits"
               :transform (str "translate(32, 32)")})))
 
 (def in-syn-svg
-  (-> d3 (.select "#viz-node") (.select "svg")
+  (-> d3 (.select "#viz") (.select "svg")
       (.append "g")
       (.attr {:id "in-syn"
               :transform (str "translate(40, 32)")})))
 
 (def rgn-svg
-  (-> d3 (.select "#viz-node") (.select "svg")
+  (-> d3 (.select "#viz") (.select "svg")
       (.append "g")
       (.attr {:id "rgn"
               :transform (str "translate(240, 32)")})))
 
-(def input-state (atom (vec (repeat n-in-items (/ numb-max 2)))))
-(def r-state (atom r-init))
 
-
-(defn update-inbits [data]
+(defn d3-draw-inbits [data]
   ;; DATA JOIN
   (let [bits (-> inbits-svg (.selectAll "g.inbit")
                  (.data data identity))]
@@ -93,13 +117,13 @@
         (.append "rect")
         (.attr {:width bitpx
                 :height bitpx})
-        (.style {:fill "yellow"
+        (.style {:fill "grey"
                  :stroke "black"}))
 
     ;; EXIT
     (-> bits (.exit)
         (.select "rect")
-        (.style {:fill "black"}))))
+        (.style {:fill "grey"}))))
 
 (defn overlap-frac
   [x]
@@ -108,17 +132,19 @@
       (min 0.95)
       (max 0.05)))
 
-(defn update-rgn [data]
+(defn d3-draw-rgn [data]
   ;; DATA JOIN
   (let [cols (-> rgn-svg (.selectAll "g.rgn")
                  (.data data first))]
     ;; UPDATE
     (-> cols
         (.select "circle")
-        (.attr {:r (inc (/ bitpx 2))})
+        (.attr {:r (/ bitpx 2)})
         (.style {:fill (fn [[_ x]]
-                         (if (= :active x) "yellow"
-                             (-> d3 (.hsl 0 1 (overlap-frac x)))))}))
+                         (cond
+                          (= x :active) "yellow"
+                          (= x :inactive) "white"
+                          :else (-> d3 (.hsl 0 1 (- 1.0 (overlap-frac x))))))}))
 
     ;; ENTER
     (-> cols (.enter)
@@ -126,67 +152,59 @@
         (.attr {:class "rgn"
                 :transform #(str "translate(0," (* (first %) bitpx) ")")})
         (.append "circle")
-        (.attr {:r (inc (/ bitpx 2))})
-        (.style {:fill "yellow"
+        (.attr {:r 1})
+        (.style {:fill "white"
                  :stroke "black"}))
 
     ;; EXIT
     (-> cols (.exit)
         (.select "circle")
         (.transition)
-        (.duration 750)
+        (.duration 500)
         (.attr {:r 1})
-        (.style {:fill "black"}))))
+        (.style {:fill "white"}))))
 
-(defn update-in-synapses [data]
+(defn d3-draw-insynapses [data]
   ;; DATA JOIN
   (let [elems (-> in-syn-svg (.selectAll "line.in-syn")
-                  (.data data (comp str first)))]
+                  (.data data (fn [[[idx col] _]] (str idx "," col))))
+        syn-stroke (fn [[[idx col] x]]
+                     (cond
+                      (= :active x) "yellow"
+                      :else (-> d3 (.hsl 0.5 1 (- 1.0 x)))))
+        syn-opacity (fn [[_ x]]
+                      (if (= :active x) 1.0
+                          0.5))]
     ;; UPDATE
     (-> elems
-        (.select "line")
-        (.style {:stroke (fn [[[idx col] state]]
-                         (if (= :active state) "yellow"
-                             (-> d3 (.hsl 0.5 1 state))))}))
+        (.style {:stroke syn-stroke
+                 :stroke-opacity syn-opacity}))
 
     ;; ENTER
     (-> elems (.enter)
         (.append "line")
         (.attr {:class "in-syn"
-                :transform (fn [[[idx col] state]]
+                :transform (fn [[[idx col] x]]
                              (str "translate(0," (* idx bitpx) ")"))
                 :x2 0
                 :y2 0})
-        (.style {:stroke (fn [[[idx col] state]]
-                         (if (= :active state) "yellow"
-                             (-> d3 (.hsl 0.5 1 state))))})
+        (.style {:stroke syn-stroke
+                 :stroke-opacity syn-opacity})
         (.transition)
-        (.duration 750)
+        (.duration 500)
         (.attr {:x2 200
-                :y2 (fn [[[idx col] state]]
-                      (- (* idx bitpx)
-                         (* col bitpx)))
+                :y2 (fn [[[idx col] x]]
+                      (- (* col bitpx)
+                         (* idx bitpx)))
                 }))
 
     ;; EXIT
     (-> elems (.exit)
-        (.select "line")
         (.transition)
-        (.duration 750)
+        (.duration 500)
         (.attr {:x2 0})
         (.attr {:y2 0})
         (.remove))))
-
-(defn sim-step!
-  []
-  (let [newin (swap! input-state (partial add-noise 5))
-        newbits (efn newin)]
-    (println (str "inputs " newin))
-    (println (str "bits " newbits))
-    (let [newr (swap! r-state p/pooling-step newbits)
-          newom (:overlaps newr)
-          newac (:active-columns newr)]
-      (println (str "ac " newac)))))
 
 ;; use core.async to run simulation separately from animation
 
@@ -196,27 +214,15 @@
                    (fn [e] (put! out e)))
     out))
 
-
-(def sim-go? (atom false))
-(def sim-step-ms (atom 1000))
-(def anim-step-ms (atom 1000))
-
-(defn run-sim
-  []
-  (go
-   (while @sim-go?
-     (sim-step!)
-     (<! (timeout @sim-step-ms)))))
-
 (defn handle-sim-control
   []
   (let [btn (dom/getElement "sim-control")
         clicks (listen btn "click")]
     (go (while true
-          (<! clicks)
-          (let [newval (swap! sim-go? not)]
-            (set! (.-innerHTML btn)
-                  (if newval "Stop sim" "Start sim"))
+          (let [e (<! clicks)
+                newval (swap! sim-go? not)]
+            (set! (.-innerHTML (.-currentTarget e))
+                  (if newval "Stop" "Start"))
             (when newval (run-sim)))))))
 
 (defn handle-sim-step
@@ -227,32 +233,91 @@
           (<! clicks)
           (sim-step!)))))
 
-(defn anim-step
+(defn animation-step!
   []
   (let [newbits (efn @input-state)
-        newr @r-state]
-    (update-inbits (vec newbits))
+        newr @r-state
+        o @display-options]
+    (d3-draw-inbits (vec newbits))
     (let [ac (:active-columns newr)
           om (:overlaps newr)
           am (zipmap ac (repeat :active))
-          st (merge om am)]
-      (update-rgn (vec st))
-      (let [syns (->> (select-keys (:columns newr) (keys om))
-                      (mapcat (fn [[id col]]
-                                (map (fn [[inp perm]]
-                                       [[inp id] perm])
-                                     (-> col :in-synapses :connected
-                                         (select-keys newbits))))))]
-        (update-in-synapses syns)))))
+          em (zipmap (map :id (:columns newr)) (repeat :inactive))
+          m (cond-> {}
+                    (:display-inactive-columns o) (merge em)
+                    (:display-overlap-columns o) (merge om)
+                    (:display-active-columns o) (merge am))]
+      (d3-draw-rgn (vec m))
+      (let [syn-cols (select-keys (:columns newr) (keys m))
+            syn-data (->> syn-cols
+                          (mapcat (fn [[col-id col]]
+                                    (let [syns (cond-> {}
+                                                       (:display-connected-insyns o)
+                                                       (merge (-> col :in-synapses :connected))
+                                                       (:display-disconnected-insyns o)
+                                                       (merge (-> col :in-synapses :disconnected))
+                                                       true
+                                                       (select-keys newbits))]
+                                      (map (fn [[in-id perm]]
+                                             [[in-id col-id] perm])
+                                           syns)))))]
+        (d3-draw-insynapses syn-data)))))
+
+(defn init-display
+  []
+  (d3-draw-inbits (vec (range bit-width)))
+  (d3-draw-rgn (mapv vector (range ncol) (repeat :inactive))))
 
 (defn run-animation
   []
   (go
-   (while true
-     (anim-step)
-     (<! (timeout @anim-step-ms)))))
+   (while @animation-go?
+     (animation-step!)
+     (<! (timeout @animation-step-ms)))))
+
+(defn handle-animation-control
+  []
+  (let [btn (dom/getElement "animation-control")
+        clicks (listen btn "click")]
+    (go (while true
+          (let [e (<! clicks)
+                newval (swap! animation-go? not)]
+            (set! (.-innerHTML (.-currentTarget e))
+                  (if newval "Stop" "Start"))
+            (when newval (run-animation)))))))
+
+(defn handle-animation-step
+  []
+  (let [btn (dom/getElement "animation-step")
+        clicks (listen btn "click")]
+    (go (while true
+          (<! clicks)
+          (animation-step!)))))
+
+(defn handle-display-options
+  []
+  (let [ids ["display-connected-insyns"
+             "display-disconnected-insyns"
+             "display-active-columns"
+             "display-overlap-columns"
+             "display-inactive-columns"]
+        btns (map dom/getElement ids)
+        cs (map listen btns (repeat "click"))
+        cm (zipmap cs ids)]
+    (go (while true
+          (let [[e c] (alts! (keys cm))
+                id (cm c)
+                on? (.-checked (.-currentTarget e))]
+            (swap! display-options assoc (keyword id) on?))))))
+
+;; TODO click on columns to filter display
+;; TODO control to turn on all inputs (to see all synapses)
+;; TODO allow stepping back in time
+;; TODO text info output
 
 (handle-sim-control)
 (handle-sim-step)
-(run-animation)
-
+(handle-animation-control)
+(handle-animation-step)
+(handle-display-options)
+(init-display)
