cbd04942c091362640d8423bad3cf7dc2e1444b1
Felix Andrews <felix@nfrac.org>
2014-11-13 13:11:59 +0800

    v0.0.6

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject comportexviz "0.0.6-SNAPSHOT"
+(defproject comportexviz "0.0.6"
   :description "Web visualisation of HTM algorithm as implemented in comportex"
   :url "http://github.com/nupic-community/comportexviz/"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2371"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [org.nfrac/comportex "0.0.6-SNAPSHOT"]
+                 [org.nfrac/comportex "0.0.6"]
                  [rm-hull/monet "0.2.1"]
                  [com.keminglabs/c2 "0.2.4-SNAPSHOT"]]
   
