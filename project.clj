c5638548ec0d6186b2f717b987d2b9b90c6186a1
Felix Andrews <felix@nfrac.org>
2016-09-01 18:25:14 +0800

    update deps.

--- project.clj
+++ project.clj
@@ -1,25 +1,25 @@
-(defproject org.numenta/sanity "0.0.14-SNAPSHOT"
+(defproject org.numenta/sanity "0.0.15-SNAPSHOT"
   :description "Web visualisation of the HTM algorithm"
   :url "https://github.com/nupic-community/sanity"
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.7.228"]
-                 [org.clojure/core.async "0.2.374"]
-                 [tailrecursion/cljs-priority-map "1.1.0"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
+                 [org.clojure/clojurescript "1.9.227"]
+                 [org.clojure/core.async "0.2.385"]
+                 [tailrecursion/cljs-priority-map "1.2.0"]
                  [org.nfrac/comportex "0.0.15-SNAPSHOT"]
-                 [rm-hull/monet "0.2.1"]
-                 [reagent "0.5.0"]
-                 [reagent-forms "0.5.6"]
-                 [ring/ring-core "1.4.0"]
-                 [compojure "1.4.0"]
-                 [info.sunng/ring-jetty9-adapter "0.9.1"]
-                 [com.cognitect/transit-clj "0.8.285"]
-                 [com.cognitect/transit-cljs "0.8.232"]
+                 [rm-hull/monet "0.3.0"]
+                 [reagent "0.5.1"]
+                 [reagent-forms "0.5.25"]
+                 [ring/ring-core "1.5.0"]
+                 [compojure "1.5.1"]
+                 [info.sunng/ring-jetty9-adapter "0.9.5"]
+                 [com.cognitect/transit-clj "0.8.288"]
+                 [com.cognitect/transit-cljs "0.8.239"]
                  [com.mrcslws/gorilla-repl "0.3.5-010"
                   :exclusions [javax.servlet/servlet-api
                                org.slf4j/slf4j-api]]]
 
-  :plugins [[lein-cljsbuild "1.1.1"]
+  :plugins [[lein-cljsbuild "1.1.3"]
             [com.cemerick/austin "0.1.6"]
             [org.clojure/tools.nrepl "0.2.10"]]
   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]
@@ -44,16 +44,15 @@
                                     :source-map "public/demos/out/sanity.js.map"}}}}}
              :debug
              {;; for Cursive debugging
-              :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005"]
-              }}
+              :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005"]}}
+
 
   :cljsbuild {:builds
               {:demos {:source-paths ["src" "examples/demos"]
                        :compiler {:output-dir "public/demos/out"
-                                  :output-to "public/demos/out/sanity.js"}}
+                                  :output-to "public/demos/out/sanity.js"}}}})
                ;; :inh {:source-paths ["src" "examples/local_inhibition"]
                ;;       :compiler {:optimizations :whitespace
                ;;                  :output-dir "public/local_inhibition/out"
                ;;                  :source-map "public/local_inhibition/out/sanity_inh.js.map"
-               ;;                  :output-to "public/local_inhibition/out/sanity_inh.js"}}
-               }})
+               ;;                  :output-to "public/local_inhibition/out/sanity_inh.js"}}})
